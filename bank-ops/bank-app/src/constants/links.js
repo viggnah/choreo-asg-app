0afb30535e37b168b918fa97d9eb2b5a3ac61072
viggnah <viggnahasd@gmail.com>
2023-06-26 18:57:49 +0530

    Ballerina exchange svc & MI integration

--- bank-ops/bank-app/src/constants/links.js
+++ bank-ops/bank-app/src/constants/links.js
@@ -10,9 +10,9 @@
 export const AsgardeoConfig = {
     signInRedirectURL: "http://localhost:3000",
     signOutRedirectURL: "http://localhost:3000",
-    clientID:"98Q7gJxVXYLE8s9K7RcF9nXl5b0a",
-    baseUrl: "https://api.asgardeo.io/t/pamod",
-    scope: ["openid", "groups", "profile"]
+    clientID:"OBx_6b2ADFiL07QGlR99Qzvw1U4a",
+    baseUrl: "https://api.asgardeo.io/t/viggnah",
+    scope: ["openid", "email", "groups", "profile", "urn:viggnah:loyaltyserviceloyaltyapi5:add_points", "urn:viggnah:loyaltyserviceloyaltyapi5:view_all_points"]
 };
 
 //Loyalty Service Host and Port for PamodORG
@@ -21,5 +21,5 @@ export const AsgardeoConfig = {
 // };
 
 export const Hosts = {
-    Loyalty: "https://70523a4f-2a9d-4e13-bb3a-92298a62b410-dev.e1-us-east-azure.choreoapis.dev/gpnq/loyaltyapplicationservice/loyalty-api-5c6/1.0.0"
+    Loyalty: "https://e767ed37-eb8c-43d6-aa4f-0a1bb02aa687-dev.e1-eu-north-azure.choreoapis.dev/nqbp/loyaltyservice/loyalty-api-5c6/1.0.0"
  };
---
+++ bank-ops/bank-conversion/service.bal
@@ -0,0 +1,21 @@
+import ballerina/http;
+import ballerina/random;
+
+service /exchange on new http:Listener(9090) {
+
+    resource function get ugx() returns http:Ok|error {
+         return {
+            body: {
+                rate: 3650 + (check random:createIntInRange(-50, 50))
+            }
+         };
+    }
+
+    resource function get mwk() returns http:Ok|error {
+        return {
+            body: {
+                rate: 1000 + (check random:createIntInRange(-40, 40))
+            }
+        };
+    }
+}
